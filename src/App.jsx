ddf39ea67bf3f7c24667692aecbea8d4710a70cb
nepalcodinghouse <nepalcodinghouse@gmail.com>
2026-02-03 20:59:26 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import TreeExperience from "./components/TreeExperience";
-
-export default function App() {
-  return <TreeExperience />;
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import TreeExperience from './components/TreeExperience'
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TreeExperience />} />
+      </Routes>
+    </Router>
+  )
 }
+
+export default App
